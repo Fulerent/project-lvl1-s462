aca953833b1103734b33aed6ca237a3c4fe6a5ab
fuleret <foll11@yandex.ru>
2019-03-20 14:22:57 +0300

    brain even ver.2.1.1

--- src/index.js
+++ src/index.js
@@ -5,40 +5,33 @@ const inputName = () => {
   console.log(`Hello, ${firstName}!`);
   return firstName;
 };
-/* Генирируем случайное число от 1 до 99 */
-const generationNumber = () => Math.floor(1 + Math.random() * (99 + 1 - 1));
+/* Генирируем случайное число */
+const generationNumber = (min = 1, max = 99) => Math.floor(1 + Math.random() * (max + min - min));
 /* Выводим это число */
-const askQuestion = (number) => {
+const printQuestion = (number) => {
   console.log(`Question: ${number}`);
   return number;
 };
-/* Спрашиваем и сохраняем ответ */
-const askAnswer = () => readlineSync.question('Your answer? ');
 /* Проверка правильности ответа */
-// eslint-disable-next-line consistent-return
-const checkAnswer = (ask, answer) => {
-  if (answer === 'yes' && ask % 2 !== 0) return false;
-  if (answer === 'no' && ask % 2 === 0) return false;
-  if (answer === 'no' && ask % 2 !== 0) return true;
-  if (answer === 'yes' && ask % 2 === 0) return true;
+const isResult = (ask, answer) => {
+  if (answer === 'no' && ask % 2 !== 0 || answer === 'yes' && ask % 2 === 0) return true;
+  return false; // не знаю, как в одну строку проверить ответ пользователя
 };
 /* Основная функция */
 const gameOfBrains = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
   const name = inputName();
-  let i = 0;
-  for (i; i < 3;) {
-    const ask = askQuestion(generationNumber());
-    const answer = askAnswer();
-    if (checkAnswer(ask, answer, name)) {
+  for (let i = 0; i < 3; i += 1) {
+    const numberIsQuestion = printQuestion(generationNumber(1, 99));
+    const userAnswer = readlineSync.question('Your answer? ');
+    if (isResult(numberIsQuestion, userAnswer)) {
       console.log('Correct!');
-      i += 1; // инкрементация происходит здесь, т.к. в другом случаи, мы просто выходим
     } else {
-      return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${name}!'`);
+      return console.log(`'no' is wrong answer ;(.\nCorrect answer was 'yes'. Let's try again, ${name}!'`);
     }
   }
-
-  if (i === 3) return console.log(`Congratulations, ${name}!`); // проверка на кол-во ответов
-  return false;
+  console.log(`Congratulations, ${name}!`);
 };
 
 export default gameOfBrains;
