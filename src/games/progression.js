c32e7c227296755b12584f65c61340c354569387
Kirill <foll11@yandex.ru>
2019-03-24 21:04:55 +0300

    finish project 1

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,7 +2,6 @@ import runCoreGame from '..';
 import generateNumber from '../generic';
 
 const descriptionGame = 'What number is missing in the progression?';
-const amountOfElements = 10;
 
 const generationProgression = (start, step, length) => {
   const progression = [];
@@ -15,13 +14,13 @@ const generationProgression = (start, step, length) => {
 const getData = () => {
   const firstNumber = generateNumber(1, 9);
   const step = generateNumber(1, 9);
-  const indexReplace = generateNumber(1, amountOfElements - 1);
-  const progression = generationProgression(firstNumber, step, amountOfElements);
-  const answer = String(progression[indexReplace]);
-  progression[indexReplace] = '..';
+  const lengthProgression = 10;
+  const replaceIndex = generateNumber(1, lengthProgression - 1);
+  const progression = generationProgression(firstNumber, step, lengthProgression);
+  const answer = String(progression[replaceIndex]);
+  progression[replaceIndex] = '..';
   const question = `${progression.join(' ')}`;
   return { question, answer };
 };
-
 
 export default () => runCoreGame(descriptionGame, getData);
